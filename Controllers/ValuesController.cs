61d32ed2329c47bb010cb8cfd86684d672401ade
Pete Capra <pcapra@expedia.com>
2017-05-12 14:26:53 +1000

    Removed redundant endpoints

--- Controllers/ValuesController.cs
+++ Controllers/ValuesController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rest_ms.Controllers
@@ -20,25 +17,8 @@ namespace dotnet_rest_ms.Controllers
         [HttpGet("{id}")]
         public string Get(int id)
         {
-            return "value";
+            return id.ToString();
         }
 
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
-        }
     }
 }
