18476f10a97cd4209301178621ee30f7c9486253
Pete Capra <pcapra@expedia.com>
2017-05-12 14:30:23 +1000

    Added support for measuring routes

--- Startup.cs
+++ Startup.cs
@@ -1,12 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc;
 
 namespace dotnet_rest_ms
 {
@@ -33,7 +30,7 @@ namespace dotnet_rest_ms
                 .AddMetricsMiddleware();
 
             // Add framework services.
-            services.AddMvc();
+            services.AddMvc( options => options.AddMetricsResourceFilter());
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
